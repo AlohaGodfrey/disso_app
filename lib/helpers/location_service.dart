01a9b79f09dcf9975f966a158713226e1de2a655
AlohaGodfrey <60743090+AlohaGodfrey@users.noreply.github.com>
2022-03-13 21:45:13 +0000

    readded MapBox api

--- lib/helpers/location_service.dart
+++ lib/helpers/location_service.dart
@@ -6,9 +6,10 @@ import 'package:http/http.dart' as http;
 // import 'package:universal_html/js.dart' as js;
 // import 'package:google_place/google_place.dart';
 import 'package:flutter/foundation.dart';
+import 'package:geocode/geocode.dart';
 
 import '../models/http_exception.dart'; //custom exception
-// import 'package:mapbox_search/mapbox_search.dart ' as mpbx;
+import 'package:mapbox_search/mapbox_search.dart ' as mpbx;
 // import 'package:mapbox_search/mapbox_search.dart ';
 
 //API KEYS
@@ -56,15 +57,8 @@ class LocationService {
     if (kIsWeb) {
       //runs api for the web (MapBox) uses a different api due to the
       //CORS blocking on web local host using standard sdk
-      // result = await getWebPlaceId(input);
-      result = {
-        'geometry': {
-          'location': {
-            'lat': 51.7,
-            'lng': -0.7,
-          }
-        }
-      };
+      //codemagic broke MapBox lol. kmt.-_-
+      result = await getWebPlaceId(input);
     } else {
       //runs api for mobile (google)
       final placeId = await getPlaceId(input);
@@ -78,75 +72,108 @@ class LocationService {
     return result;
   }
 
-  //web
   // static Future<Map<String, dynamic>> getWebPlaceId(String input) async {
-  //   //uses the mapbox forward geocoding api to get latlng from text query
-  //   Map<String, dynamic> nomalizedResponse;
+  //   GeoCode geoCode = GeoCode(apiKey: '448156264906954585637x48800');
 
-  //   var placesSearch = PlacesSearch(
-  //     apiKey: MAPBOX_API_KEY,
-  //     limit: 5,
-  //   );
+  //   //default coord in case of failure
+  //   Map<String, dynamic> normalizedResponse;
 
   //   try {
-  //     //feteches the data
-  //     final response = await placesSearch.getPlaces(input);
-
-  //     final lat = response![0].geometry!.coordinates![1];
-  //     final lng = response[0].geometry!.coordinates![0];
-  //     nomalizedResponse = {
+  //     Coordinates coordinates = await geoCode.forwardGeocoding(address: input);
+  //     normalizedResponse = {
   //       'geometry': {
   //         'location': {
-  //           'lat': lat,
-  //           'lng': lng,
+  //           'lat': coordinates.latitude,
+  //           'lng': coordinates.longitude,
   //         }
   //       }
   //     };
-  //   } catch (error) {
-  //     //if error the coords default to Trafalgar Square
-  //     nomalizedResponse = {
+  //     print("Latitude: ${coordinates.latitude}");
+  //     print("Longitude: ${coordinates.longitude}");
+  //   } catch (e) {
+  //     print(e);
+  //     normalizedResponse = {
   //       'geometry': {
   //         'location': {
-  //           'lat': 51.507321899999994,
-  //           'lng': -0.12764739999999997,
+  //           'lat': 51.7,
+  //           'lng': -0.7,
   //         }
   //       }
   //     };
   //   }
 
-  //   return nomalizedResponse;
+  //   return normalizedResponse;
   // }
 
-  // static Future<void> getCurrentuserLocation() async {
-  //   Location location = Location();
+  // web
+  static Future<Map<String, dynamic>> getWebPlaceId(String input) async {
+    //uses the mapbox forward geocoding api to get latlng from text query
+    Map<String, dynamic> nomalizedResponse;
 
-  //   bool _serviceEnabled;
-  //   PermissionStatus _permissionGranted;
+    var placesSearch = mpbx.PlacesSearch(
+      apiKey: MAPBOX_API_KEY,
+      limit: 5,
+    );
 
-  //   _serviceEnabled = await location.serviceEnabled();
-  //   if (!_serviceEnabled) {
-  //     _serviceEnabled = await location.requestService();
-  //     if (!_serviceEnabled) {
-  //       return;
-  //     }
-  //   }
+    try {
+      //feteches the data
+      final response = await placesSearch.getPlaces(input);
 
-  //   _permissionGranted = await location.hasPermission();
-  //   if (_permissionGranted == PermissionStatus.denied) {
-  //     _permissionGranted = await location.requestPermission();
-  //     if (_permissionGranted != PermissionStatus.granted) {
-  //       return;
-  //     }
-  //   }
+      final lat = response![0].geometry!.coordinates![1];
+      final lng = response[0].geometry!.coordinates![0];
+      nomalizedResponse = {
+        'geometry': {
+          'location': {
+            'lat': lat,
+            'lng': lng,
+          }
+        }
+      };
+    } catch (error) {
+      //if error the coords default to Trafalgar Square
+      nomalizedResponse = {
+        'geometry': {
+          'location': {
+            'lat': 51.507321899999994,
+            'lng': -0.12764739999999997,
+          }
+        }
+      };
+    }
 
-  //   final locData = await location.getLocation();
+    return nomalizedResponse;
+  }
 
-  //   //all you need is latitute and longtitute for any location in the world
-  //   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //   if (locData == null) {
-  //     return;
-  //   }
+  static Future<void> getCurrentuserLocation() async {
+    Location location = Location();
 
-  //   print(locData);
-  // }
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final locData = await location.getLocation();
+
+    //all you need is latitute and longtitute for any location in the world
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    if (locData == null) {
+      return;
+    }
+
+    print(locData);
+  }
 }
